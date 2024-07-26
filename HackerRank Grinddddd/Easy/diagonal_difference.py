c00274732c503cd720272d1d488ac1609867fc95
Brianna Hinds <briden32016@gmail.com>
2024-07-26 08:27:03 -0500

    hackerrank

---
+++ HackerRank Grinddddd/Easy/diagonal_difference.py
@@ -0,0 +1,31 @@
+"""
+Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+"""
+
+def diag_diff(arr):
+    print(len(arr))
+    left_right = 0
+    right_left = 0
+
+    # time complexity of O(N)
+    for i in range(0, len(arr)):
+        # get left to right values: 00 -> 11 -> 22
+        """
+        The values at i and j are the same value so we just want to pull one since i = j
+        """
+        left_right += arr[i][i]
+        print(f"value at [{i}][{i}]: {arr[i][i]} adding to left_right")
+
+        # get right to left values (ij): 20 -> 11 -> 02, we can find j by taking the absolute value of i-len(arr)-1
+        right_left += arr[abs(i-(len(arr)-1))][i]
+        print(f"value at [{i}][{abs(i-(len(arr)-1))}]: {arr[i][abs(i-(len(arr)-1))]} adding to left_right")
+
+
+    return abs(left_right-right_left)
+
+
+if __name__ == "__main__":
+    arr = [[1, 2, 3], 
+           [4, 5, 6], 
+           [9, 8, 9]]  # output should be: 2 = |(1+5+9)-(3+5+9)|
+    print(diag_diff(arr))
